c0d99070684305fd7a499dd775a8d296d44d4d93
nguyenanhnhan95 <nguyenanhnhan95@gmail.com>
2023-04-06 10:31:49 +0700

    Em nop bai tap

--- ss3_array_method/bai_tap/Prime.java
+++ ss3_array_method/bai_tap/Prime.java
@@ -5,17 +5,14 @@ import java.util.Scanner;
 public class Prime {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int number;
-        do {
-            System.out.printf("Enter number check prime greater one:");
-            number = Integer.parseInt(sc.nextLine());
-        } while (number < 2);
+        System.out.printf("Enter number check prime :");
+        int number = Integer.parseInt(sc.nextLine());
         System.out.println(checkPrime(number));
     }
 
     public static boolean checkPrime(int number) {
-        if (number <= 2) {
-            return true;
+        if (number < 2) {
+            return false;
         }
         for (int i = 2; i < number; i++) {
             if (number % i == 0) {
